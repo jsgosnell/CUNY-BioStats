aa610beaf9551cee4c918640a42a0dd44eb52a0f
J. Stephen Gosnell <stephengosnell@gmail.com>
2017-09-14 08:31:09 -0400

    adding binomial example

--- Examples/central_limit_theorem.R
+++ Examples/central_limit_theorem.R
@@ -56,3 +56,16 @@ for (j in 1:length(n)){
   }
   hist(means, main=paste("Chi square, n = ",n[j], sep = ""))
 }
+
+#binomial
+n=c(1,5,10,20,40,80)
+means=1:9000
+par(mfrow=c(2,3), ask=F)
+for (j in 1:length(n)){
+  for (i in 1:length(means)){
+    means[i]=mean(rbinom(n=n[j], 100, .7))
+  }
+  hist(means, main=paste("Binomial, n = ",n[j], sep = ""), col = "orange")
+}
+mtext("Draws from a binomial distribution, p = .7", outer = T, side = 1, 
+      cex = 3, line = 1)
