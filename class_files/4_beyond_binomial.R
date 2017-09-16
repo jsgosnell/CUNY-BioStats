892131b261b87e300983da0b69935da773fa9d74
Stephen Gosnell <stephen.gosnell@baruch.cuny.edu>
2017-09-15 23:07:34 -0400

    working on graphs

--- class_files/4_beyond_binomial.R
+++ class_files/4_beyond_binomial.R
@@ -21,13 +21,62 @@ ggplot(days_of_week_births, aes_string(x= "Days", y = "Births")) +
 using_distribution = dchisq(0:50,6, 1/7)
 using_distribution
 sum(using_distribution)
-Number_righthanded = c(0:50)
+x = c(0:50)
 pdf = data.frame(Number_righthanded, using_distribution)
-plot(0:50, using_distribution)
-#for all plots, R takes a best guess at the best type of plot; you can also
-#direct it with the type command.  ?plot for more info
-barplot(using_distribution,
-        xlab = "Chi-squared distribution", ylab = "Probability density" )
+
+require(ggplot2)
+
+#just histogram
+ggplot(pdf, aes_string(x="x", y = "using_distribution")) +
+  geom_col(fill = "orange") +
+  ylab("Probablity") +
+  xlab("")+
+  theme(axis.title.x = element_text(face="bold", size=28), 
+        axis.title.y = element_text(face="bold", size=28), 
+        axis.text.y  = element_text(size=20),
+        axis.text.x  = element_text(size=20), 
+        legend.text =element_text(size=20),
+        legend.title = element_text(size=20, face="bold"),
+        plot.title = element_text(hjust = 0.5, face="bold", size=32))
+
+#with curve
+ggplot(pdf, aes_string(x="x", y = "using_distribution")) +
+  geom_col(fill = "orange") +
+  stat_function(fun = dchisq, args = list(df = 6),size = 3, color = "green") + 
+  ylab("Probablity") +
+  xlab("")+
+  theme(axis.title.x = element_text(face="bold", size=28), 
+        axis.title.y = element_text(face="bold", size=28), 
+        axis.text.y  = element_text(size=20),
+        axis.text.x  = element_text(size=20), 
+        legend.text =element_text(size=20),
+        legend.title = element_text(size=20, face="bold"),
+        plot.title = element_text(hjust = 0.5, face="bold", size=32))
+
+#filled in
+
+dnorm_one_sd <- function(x){
+  norm_one_sd <- dchisq(x)
+  # Have NA values outside interval x in [-1, 1]:
+  norm_one_sd[x >= qchisq(.95, df = 6)] <- NA
+  return(norm_one_sd)
+}
+
+
+ggplot(pdf, aes_string(x="x", y = "using_distribution")) +
+  stat_function(fun = dchisq, args = list(df = 6),size = 3, fill = "green", geom = "area") + 
+  stat_function(fun = dnorm_one_sd, args = list(df = 6), size = 3, fill = "blue",
+                geom = "area") + 
+  ylab("Probablity") +
+  xlab("")+
+  theme(axis.title.x = element_text(face="bold", size=28), 
+        axis.title.y = element_text(face="bold", size=28), 
+        axis.text.y  = element_text(size=20),
+        axis.text.x  = element_text(size=20), 
+        legend.text =element_text(size=20),
+        legend.title = element_text(size=20, face="bold"),
+        plot.title = element_text(hjust = 0.5, face="bold", size=32))
+
 
 
 
