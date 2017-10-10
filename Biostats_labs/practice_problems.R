47a9222c0dcd05de31dda166097fa0d598bc3506
Stephen Gosnell <stephen.gosnell@baruch.cuny.edu>
2017-10-10 13:22:53 -0400

    adding problems

--- Biostats_labs/practice_problems.R
+++ Biostats_labs/practice_problems.R
@@ -20,4 +20,34 @@ fisher.test(matrix(c(16,8,3,18), nrow=2, byrow = T))
 #pvalue is .004541
 ##given this data, we reject HO.  there is enough data to suggest the 
 #proportion of chipmunks trilling does depend on distance.
-
+#
+#Australian male athlete weight####
+sport <- read.csv("https://sites.google.com/site/stephengosnell/teaching-resources/datasets/sport.csv?attredirects=0&d=1")
+##HO: average weight of males training at the facility is no different than average weight of australian male
+#HA: average weight of males training at the facility is different than average weight of australian male
+##normal based method
+t.test(sport[sport$Sex == "male", "Wt"], mu = 85.9)
+#p value is .007089
+#given this data, we reject HO.there is enough data to suggest that 
+#average weight of males training at the facility is different than average weight of australian male
+##bootstrap method (no assumptions)
+require(simpleboot)
+bootstrapjsg=function(data1, data2=NULL, conf=.95, fun=mean, r=10000, null=0)
+{
+  if (is.null(data2)){
+    a=one.boot(na.omit(data1), fun, r)
+    confint=boot.ci(a, conf)
+    p=length(subset(a$t, abs(a$t-mean(a$t))>=abs(null-mean(a$t))))/r
+    output=c(conf, "% Confidence Interval", confint$percent[1,4:5], "p-value", p)
+    return(output)}
+  if (is.null(data2)==F){
+    a=two.boot(na.omit(data1), na.omit(data2), fun, r)
+    confint=boot.ci(a, conf)
+    p=length(subset(a$t, abs(a$t-mean(a$t))>=abs(null-mean(a$t))))/r
+    output=c(conf, "% Percentile Confidence Interval", confint$percent[1,4:5], "p-value", p)
+    return(output)}
+}
+bootstrapjsg(data1=sport[sport$Sex == "male", "Wt"], null=85.9)
+#p value is .0054
+#given this data, we reject HO.there is enough data to suggest that 
+#average weight of males training at the facility is different than average weight of australian male
