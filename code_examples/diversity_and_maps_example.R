c1cb60325a3cb259ca31fd51f769a31291a08a27
Stephen Gosnell <stephen.gosnell@baruch.cuny.edu>
2017-11-29 23:43:30 -0500

    updating

--- code_examples/diversity_and_maps_example.R
+++ code_examples/diversity_and_maps_example.R
@@ -1,5 +1,6 @@
 #example code to work with diversity data and map it
-
+#pull all diversity files to your desktop and set working directory there
+setwd("C:/Users/Stephen/Desktop")
 #notes on data manipulation####
 #long to wide
 cholesterol <- read.table("http://www.statsci.org/data/general/cholestg.txt", header = T)
@@ -41,14 +42,14 @@ cholesterol_long <- melt(cholesterol, id.vars =c(), variable.name = "day",
 head(cholesterol_long)
 
 #more id variables
+sport <- read.csv("https://sites.google.com/site/stephengosnell/teaching-resources/datasets/sport.csv?attredirects=0&d=1")
 sport_melted <- melt(sport, id.vars = c("Sex", "Sport"),
                      variable.name = "measure", 
                      value.name = "value")
 head(sport_melted)
 
-setwd("C:/Users/Stephen/Desktop")
-
-tree_data <- read.csv(file.choose(), 
+#team analysis####
+tree_data <- read.csv("tree_data_clean_wd_dbh.csv", 
                       na.strings=c("NA","-","", "NULL", "null", "Null", "Unknown", "unknown", 
                                "unidentifiable", "Unidentifiable"), 
                       strip.white = T)
@@ -61,7 +62,6 @@ summary(tree_data)
 #unknowns (perfect world scenario)
 
 #make wide dataframe
-require(reshape2)
 tree_data_wide=dcast(tree_data, Site.Name+X1ha.Plot.Number+Sampling.Period~Family_Genus, length)
 #check this out
 tree_data_wide[1:3,1:10]
@@ -79,13 +79,22 @@ require(BiodiversityR)
 richness <- diversityresult(abundance_data, method="each site", index = "richness")
 site_info$richness=richness$richness
 
-shannon <- diversityresult(abundance_data, method="each site", index = "Shannon")
-site_info$shannon=shannon$Shannon
-
 berger <- diversityresult(abundance_data, method="each site", index = "Berger")
 site_info$berger=berger$Berger
 
-#functional diversity measures
+simpson <- diversityresult(abundance_data, method="each site", index = "Simpson")
+site_info$simpson=simpson$Simpson
+
+inv_simpson <- diversityresult(abundance_data, method="each site", index = "inverseSimpson")
+site_info$berger=inv_simpson$inverseSimpson
+
+shannon <- diversityresult(abundance_data, method="each site", index = "Shannon")
+site_info$shannon=shannon$Shannon
+
+evenness <- diversityresult(abundance_data, method="each site", index = "Jevenness")
+site_info$jevenness=evenness$Jevenness
+
+#functional diversity measures####
 require(FD)
 #dbFD functions requires matrices that match order for species abundance/traits
 # and that have rownanes for sites and colnames for species
@@ -98,26 +107,24 @@ require(FD)
 #for wd 
 wd_trait <- aggregate(wd~Family_Genus, tree_data, mean)
 head(wd_trait)
-abundance_for_wd_fd=cast(tree_data, Site.Name+X1ha.Plot.Number+Sampling.Period~Family_Genus, length)
 
 #make sure taxa are in same order
-names(abundance_for_wd_fd)
-names(tree_data_wide)
+names(abundance_data)
 names(wd_trait)
 all.equal(wd_trait$Family_Genus, names(abundance_data),
           check.attributes = F)
 
 #now make matrix without extra columns and with rownames
 #remove extra columsn (you just want species)
-tree_data_wide_matrix <- as.matrix(abundance_data)
-rownames(tree_data_wide_matrix) <- paste(tree_data_wide$Site.Name, tree_data_wide$X1ha.Plot.Number, 
+abundance_data_matrix <- as.matrix(abundance_data)
+rownames(abundance_data_matrix) <- paste(tree_data_wide$Site.Name, tree_data_wide$X1ha.Plot.Number, 
                                          tree_data_wide$Sampling.Period, sep = "_")
-colnames(tree_data_wide_matrix)=names(abundance_data)
+colnames(abundance_data_matrix) = names(abundance_data)
 wd_trait_matrix <- as.matrix(wd_trait["wd"])
 rownames(wd_trait_matrix) <- wd_trait$Family_Genus
 
 #do the calculations
-calc_wd_fd <- dbFD(wd_trait_matrix,tree_data_wide_matrix,calc.FRic = F, calc.CWM = T, calc.FDiv = F)
+calc_wd_fd <- dbFD(wd_trait_matrix,abundance_data_matrix,calc.FRic = T, calc.CWM = T, calc.FDiv = T)
 
 calc_wd_fd=as.data.frame(calc_wd_fd)
 names(calc_wd_fd)
@@ -134,7 +141,7 @@ site_info <- merge(site_info,calc_wd_fd, all.x=T, all.y=T)
 
 #phylogenetic diversity####
 #make tree with genus
-genus_matrix <- cast(tree_data, Site.Name+X1ha.Plot.Number+Sampling.Period~Genus,
+genus_matrix <- dcast(tree_data, Site.Name+X1ha.Plot.Number+Sampling.Period~Genus,
                      length)
 #for traits, use
 trait_matrix_genus <- aggregate(cbind(wd, max_dbh)~ Genus + Family, tree_data, mean)
@@ -143,20 +150,6 @@ require(brranching)
 tree_genus <- phylomatic (taxa=tolower(as.character(trait_matrix_genus$Genus)), storedtree='R20120829', get='POST')
 plot(tree_genus, no.margin = T)
 write.tree(tree_genus, file="TEAMtreeonlygenus.tre")
-
-# #tree with fake species (may need for some analysis, just saved here)
-# trait_matrix_species <- trait_matrix_genus
-# require(taxize)
-# for (i in 1:nrow(trait_matrix_species)){ 
-#   dummy_species <- children(as.character(trait_matrix_species$Genus)[i], db = "ncbi") 
-#   dummy_species <- as.data.frame(dummy_species[1]) 
-#   trait_matrix_species$dummy_species[i] <- dummy_species[1,2] 
-#   trait_matrix_species$dummy_species_rank[i] <- dummy_species[1,3] 
-#   print(paste(i, "out of ", nrow(trait_matrix_genus), "completed"), sep = " ") 
-# } 
-# tree_species <- phylomatic(taxa=as.character(trait_matrix_species$dummy_species), 
-#                         storedtree='R20120829', get='POST') 
-# write.tree(tree_species, file="TEAMtreeonlyspecies.tre")
 
 #age tree####
 #code below will age tree
@@ -176,8 +169,6 @@ write.tree(tree_genus, file="TEAMtreeonlygenus.tre")
   #copied wikistromages and renamed ages
   #R20100701.new (supertree)
   
-#can update/use below to add filenames if wanted, for now just entered directly
-
 #my output file
 # mycleanoutfile<- paste("phylocom_calculations/","taxafile",".clean.new",sep="")
 # #dated newick file
@@ -202,8 +193,8 @@ system(paste(getwd(),"/phylocom bladj -f ", "taxafile_clean.new"," > ",
 tree_genus_dated <- read.tree("taxafile_dated.new")
 
 #return to main working directory
+setwd("C:/Users/Stephen/Desktop")
 
-#find code Ana used for circular tree
 ##below code only needs to be run once to install ggtree from bioconductor repository
 source("http://bioconductor.org/biocLite.R") # downloads the latest bioconductor script/repos
 biocLite("ggtree")
@@ -296,11 +287,11 @@ site_info$siteyear <- paste(site_info$X1ha.Plot.Number,
 site_info <- merge(site_info, TEAM_pd, all.x=T)
 site_info <- merge(site_info, TEAM_mpd, all.x = T)
 site_info <- merge(site_info, TEAM_mpd_with_abundance, all.x = T)
-site_info <- merge(site_info, TEAM_nri, all.x = T)
+#site_info <- merge(site_info, TEAM_nri, all.x = T)
 site_info <- merge(site_info, TEAM_mntd, all.x = T)
 site_info <- merge(site_info, TEAM_mntd_with_abundance, all.x = T)
 site_info <- merge(site_info, TEAM_td, all.x = T)
-site_info <- merge(site_info, TEAM_cac, all.x = T)
+#site_info <- merge(site_info, TEAM_cac, all.x = T)
 
 #automate addingin other factors
 file="biomassandclimate.csv"
@@ -315,6 +306,9 @@ site_info <- merge(site_info,long_lat_evapotranspiration,
 
 write.csv(site_info, "tree_data_complete.csv")
 
+#pull out years we used for analysis
+site_info_most_recent_year <- site_info[is.na(site_info$Latitude) != T, ]
+
 #mapping
 #using natural earth gis layers and ggplot2
 require(rgdal)
@@ -326,31 +320,56 @@ plot(base_map)
 base_map@data
 
 require(ggplot2)
-
-mymapfortified=fortify(mymap)
+base_map_fortified=fortify(base_map)
+names(base_map_fortified)[names(base_map_fortified) == "long"] <- "Longitude"
+names(base_map_fortified)[names(base_map_fortified) == "lat"] <- "Latitude"
 #Creat a base plot
 #coord_fixed keeps scale right (1:1)
 #coord_map uses map projections
-ggplot(data=mymapfortified,aes_string(x="long",y="lat", group="group") +
+ggplot(data=base_map_fortified,aes_string(x="Longitude",y="Latitude", group="group")) +
          geom_polygon(colour="black", fill="white") +
          xlab("Longitude")+
-         ylab("Latitude")
-       
-#input collection points and use coord_fixed to zoom in
-
-ggplot(data=mymapfortified,aes_string(x="long",y="lat", group="group") +
-         geom_polygon(colour="black", fill="white") + 
-         xlab("Longitude")+
          ylab("Latitude") +
-         geom_point() +
-         coord_fixed(xlim=c(min(bysite$Longitude)-.5,max(bysite$Longitude)+.5)
+  theme(axis.title.x = element_text(face="bold", size=28), 
+        axis.title.y = element_text(face="bold", size=28), 
+        axis.text.y  = element_text(size=20),
+        axis.text.x  = element_text(size=20), 
+        legend.text =element_text(size=20),
+        legend.title = element_text(size=20, face="bold"),
+        plot.title = element_text(hjust = 0.5, face="bold", size=32))
        
-                     annotate("text", label="Florida", x=-84.75, y=30.5, size=14)+
-                       annotate("text", label="FSUCML", x=-84.72, y=29.95, size=10)+
-                       annotate("text", label="Wakulla Beach", x=-84.25, y=30.17, size=10)
-                     
-                     
-                     scaleBar(lon = -84.4, lat = 29.7, distanceLon = 25,
-                              distanceLat = 5, distanceLegend = 10, dist.unit = "km",
-                              arrow.length=20, arrow.distance=15, legend.size=5, arrow.North.size=10, arrow.size=1.25)
+#input collection points
+ggplot(data=base_map_fortified,aes_string(x="Longitude",y="Latitude", group="group")) +
+  geom_polygon(colour="black", fill="white") +
+  xlab("Longitude")+
+  ylab("Latitude") +
+  theme(axis.title.x = element_text(face="bold", size=28), 
+        axis.title.y = element_text(face="bold", size=28), 
+        axis.text.y  = element_text(size=20),
+        axis.text.x  = element_text(size=20), 
+        legend.text =element_text(size=20),
+        legend.title = element_text(size=20, face="bold"),
+        plot.title = element_text(hjust = 0.5, face="bold", size=32)) + 
+  geom_point(data = site_info_most_recent_year, 
+             aes_string(x="Longitude",y="Latitude", group = NA), size = 10)
 
+#color collection points by diversity
+#get average richness per site
+
+mean_richness <- dcast(site_info_most_recent_year, Longitude + Latitude ~ "Mean_Richness", value.var = "richness", mean)
+ggplot(data=base_map_fortified,aes_string(x="Longitude",y="Latitude", group="group")) +
+  geom_polygon(colour="black", fill="white") +
+  xlab("Longitude")+
+  ylab("Latitude") +
+  theme(axis.title.x = element_text(face="bold", size=28), 
+        axis.title.y = element_text(face="bold", size=28), 
+        axis.text.y  = element_text(size=20),
+        axis.text.x  = element_text(size=20), 
+        legend.text =element_text(size=20),
+        legend.title = element_text(size=20, face="bold"),
+        plot.title = element_text(hjust = 0.5, face="bold", size=32)) + 
+  geom_point(data = mean_richness, 
+             aes_string(x="Longitude",y="Latitude", group = NA, color = "Mean_Richness"), size = 10) +
+  scale_color_continuous(guide = guide_legend(title = "Mean richness"))
+  
+#add code for scale bar
