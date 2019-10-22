48941c19267c903dd069badf27959f0bef86ff36
Stephen <stephen.gosnell@baruch.cuny.edu>
2019-10-21 20:41:46 -0400

    updating bootstrap code

--- lecture_files/4_normality.R
+++ lecture_files/4_normality.R
@@ -250,9 +250,34 @@ ggplot(boot.md, aes_string("mean")) +
         plot.title = element_text(hjust = 0.5, face="bold", size=32))
 
 #bootstrapjsg function####
-require(simpleboot)
-bootstrapjsg=function(data1, data2=NULL, conf=.95, fun=mean, r=10000, null=0)
+bootstrapjsg <- function(data1, data2=NULL, conf=.95, fun=mean, r=10000, null=0)
 {
+  require(boot)
+  one.boot <- function (data, FUN, R, student = FALSE, M, weights = NULL, ...) 
+  {
+    func.name <- ifelse(is.character(FUN), FUN, deparse(substitute(FUN)))
+    extra <- list(...)
+    if (func.name == "quantile") {
+      if (is.na(match("probs", names(extra)))) 
+        stop("'probs' argument must be specified")
+      if (length(extra$probs) > 1) 
+        stop("can only bootstrap a single quantile")
+    }
+    func <- match.fun(FUN)
+    boot.func <- function(x, idx) {
+      fval <- func(x[idx], ...)
+      if (student) {
+        rs.x <- x[idx]
+        b <- one.boot(rs.x, FUN, R = M, student = FALSE, 
+                      M = NULL, weights = NULL, ...)
+        fval <- c(fval, var(b$t))
+      }
+      fval
+    }
+    b <- boot(data, statistic = boot.func, R = R, weights = weights)
+    b$student <- student
+    structure(b, class = "simpleboot")
+  }
   if (is.null(data2)){
     a=one.boot(na.omit(data1), fun, r)
     confint=boot.ci(a, conf)
