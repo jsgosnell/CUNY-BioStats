128fd6969e7d6176b0052d7ba15f0670e5baf77d
Stephen <stephen.gosnell@baruch.cuny.edu>
2019-11-13 09:55:46 -0500

    new file started for gamma distribution

--- lecture_files/12_more_model_extensions.R
+++ lecture_files/12_more_model_extensions.R
@@ -1,1 +1,22 @@
 #code for gamma distribution####
+#death data from crawley
+deaths <- read.csv("https://raw.githubusercontent.com/jsgosnell/CUNY-BioStats/master/datasets/deaths.csv")
+str(deaths)
+library(Rmisc)
+summarySE(deaths, measurevar = "death", groupvars = "treatment")
+#graph it####
+library(ggplot2)
+ggplot(summarySE(deaths, measurevar = "death", groupvars = "treatment"), 
+       aes ( x= treatment, y = death)) +
+  geom_col() +
+  geom_errorbar(aes(ymin = death - ci, ymax = death + ci))
+
+#wrong order!
+deaths$treatment <- factor(deaths$treatment, c("control", "low", "high"))
+
+sizes <- factor(sizes, levels = c("small", "medium", "large"))
+
+
+
+#
+#lm approach#####
