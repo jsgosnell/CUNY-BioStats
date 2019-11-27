93ae3f169d724f386f181a71d94e6f21357245ed
Stephen <Gosnell>
2019-11-27 13:23:09 -0500

    updating and including bar graph

--- practice_problems/2018 quizzes/9_2_way_anova.R
+++ practice_problems/2018 quizzes/9_2_way_anova.R
@@ -13,6 +13,16 @@
 
 
 poison <- read.csv("https://raw.githubusercontent.com/jsgosnell/CUNY-BioStats/master/datasets/poison.csv")
+str(poison)
+#rename x label
+library(plyr)
+poison$Treatment <- revalue(poison$Treatment, c("treatment_1" = "Treatment 1",
+                                                "treatment_2" = "Treatment 2",
+                                                "treatment_3" = "Treatment 3",
+                                                "treatment_4" = "Treatment 4"))
+poison$Poison <- revalue(poison$Poison, c("poison_1" = "Poison 1",
+                                                "poison_2" = "Poison 2",
+                                                "poison_3" = "Poison 3"))
 
 #graph
 #bar chart with error bars ####
@@ -23,10 +33,9 @@ function_output <- summarySE(poison, measurevar = "Time", groupvars =
 library(ggplot2)
 ggplot(function_output, aes(x=Treatment, y=Time,color=Poison, 
                                    shape = Poison)) +
-  geom_errorbar(aes(ymin=Time-ci, ymax=Time+ci), size=1.5) + #use width = to set change bar width
-  geom_line(aes(group=Poison, linetype = Poison), size=2) +
   geom_point(size = 5) +
-  #  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), size=1.5) +
+  geom_errorbar(aes(ymin=Time-ci, ymax=Time+ci), size=1.5) + 
+  geom_line(aes(group=Poison, linetype = Poison), size=2) +
   ylab("Survival Time")+ 
   xlab("Treatment") + 
   ggtitle("Poison type and treatment impact survival")+
@@ -37,6 +46,24 @@ ggplot(function_output, aes(x=Treatment, y=Time,color=Poison,
         legend.text =element_text(size=20),
         legend.title = element_text(size=20, face="bold"),
         plot.title = element_text(hjust = 0.5, face="bold", size=32))
+
+#or a bar chart
+
+ggplot(function_output, aes(x=Treatment, y=Time,fill=Poison, 
+                            shape = Poison)) +
+  geom_col(position = "dodge", size = 4, width = .5) +
+  geom_errorbar(aes(ymin=Time-ci, ymax=Time+ci), size=1.5, position = position_dodge(.5))+ 
+  ylab("Survival Time")+ 
+  xlab("Treatment") + 
+  ggtitle("Poison type and treatment impact survival")+
+  theme(axis.title.x = element_text(face="bold", size=28), 
+        axis.title.y = element_text(face="bold", size=28), 
+        axis.text.y  = element_text(size=20),
+        axis.text.x  = element_text(size=20), 
+        legend.text =element_text(size=20),
+        legend.title = element_text(size=20, face="bold"),
+        plot.title = element_text(hjust = 0.5, face="bold", size=32))
+
 
 #build model
 poison_model <- lm(Time ~ Poison*Treatment, poison)
