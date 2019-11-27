46a3cd93f6a7a2127f60e75159bc165f6ce2ee6d
Stephen <Gosnell>
2019-11-27 13:07:58 -0500

    updating

--- practice_problems/2018 quizzes/9_2_way_anova.R
+++ practice_problems/2018 quizzes/9_2_way_anova.R
@@ -16,52 +16,15 @@ poison <- read.csv("https://raw.githubusercontent.com/jsgosnell/CUNY-BioStats/ma
 
 #graph
 #bar chart with error bars ####
-summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
-                      conf.interval=.95, .drop=TRUE) {
-  library(plyr)
-  
-  # New version of length which can handle NA's: if na.rm==T, don't count them
-  length2 <- function (x, na.rm=FALSE) {
-    if (na.rm) sum(!is.na(x))
-    else       length(x)
-  }
-  
-  # This does the summary. For each group's data frame, return a vector with
-  # N, mean, and sd
-  datac <- ddply(data, groupvars, .drop=.drop,
-                 .fun = function(xx, col) {
-                   c(N    = length2(xx[[col]], na.rm=na.rm),
-                     mean = mean   (xx[[col]], na.rm=na.rm),
-                     sd   = sd     (xx[[col]], na.rm=na.rm)
-                   )
-                 },
-                 measurevar
-  )
-  
-  # Rename the "mean" column
-  #  datac <- rename(datac, c("mean" = measurevar))
-  
-  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
-  
-  # Confidence interval multiplier for standard error
-  # Calculate t-statistic for confidence interval:
-  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
-  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
-  datac$ci <- datac$se * ciMult
-  
-  return(datac)
-}
-
-#use summarySE to check for variance if wanted
-
+library(Rmisc)
 function_output <- summarySE(poison, measurevar = "Time", groupvars = 
                                c("Poison", "Treatment")) 
 
-require(ggplot2)
-ggplot(function_output, aes_string(x="Treatment", y="mean",color="Poison", 
-                                   shape = "Poison")) +
-  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), size=1.5) + #use width = to set change bar width
-  geom_line(aes_string(group="Poison", linetype = "Poison"), size=2) +
+library(ggplot2)
+ggplot(function_output, aes(x=Treatment, y=Time,color=Poison, 
+                                   shape = Poison)) +
+  geom_errorbar(aes(ymin=Time-ci, ymax=Time+ci), size=1.5) + #use width = to set change bar width
+  geom_line(aes(group=Poison, linetype = Poison), size=2) +
   geom_point(size = 5) +
   #  geom_errorbar(aes(ymin=mean-ci, ymax=mean+ci), size=1.5) +
   ylab("Survival Time")+ 
