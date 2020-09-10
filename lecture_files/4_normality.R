b15d141cec760115ef1358da58d1b38836ac2cd3
J. Stephen Gosnell <stephen.gosnell@baruch.cuny.edu>
2020-09-10 15:43:13 -0400

    changing all references to bootstrapjsg to point to one file

--- lecture_files/4_normality.R
+++ lecture_files/4_normality.R
@@ -250,47 +250,8 @@ ggplot(boot.md, aes(mean)) +
         plot.title = element_text(hjust = 0.5, face="bold", size=32))
 
 #bootstrapjsg function####
-bootstrapjsg <- function(data1, data2=NULL, conf=.95, fun=mean, r=10000, null=0)
-{
-  library(boot)
-  one.boot <- function (data, FUN, R, student = FALSE, M, weights = NULL, ...) 
-  {
-    func.name <- ifelse(is.character(FUN), FUN, deparse(substitute(FUN)))
-    extra <- list(...)
-    if (func.name == "quantile") {
-      if (is.na(match("probs", names(extra)))) 
-        stop("'probs' argument must be specified")
-      if (length(extra$probs) > 1) 
-        stop("can only bootstrap a single quantile")
-    }
-    func <- match.fun(FUN)
-    boot.func <- function(x, idx) {
-      fval <- func(x[idx], ...)
-      if (student) {
-        rs.x <- x[idx]
-        b <- one.boot(rs.x, FUN, R = M, student = FALSE, 
-                      M = NULL, weights = NULL, ...)
-        fval <- c(fval, var(b$t))
-      }
-      fval
-    }
-    b <- boot(data, statistic = boot.func, R = R, weights = weights)
-    b$student <- student
-    structure(b, class = "simpleboot")
-  }
-  if (is.null(data2)){
-    a=one.boot(na.omit(data1), fun, r)
-    confint=boot.ci(a, conf)
-    p=length(subset(a$t, abs(a$t-mean(a$t))>=abs(null-mean(a$t))))/r
-    output=c(conf, "% Confidence Interval", confint$percent[1,4:5], "p-value", p)
-    return(output)}
-  if (is.null(data2)==F){
-    a=two.boot(na.omit(data1), na.omit(data2), fun, r)
-    confint=boot.ci(a, conf)
-    p=length(subset(a$t, abs(a$t-mean(a$t))>=abs(null-mean(a$t))))/r
-    output=c(conf, "% Percentile Confidence Interval", confint$percent[1,4:5], "p-value", p)
-    return(output)}
-}
+#source from github
+source("https://raw.githubusercontent.com/jsgosnell/CUNY-BioStats/master/code_examples/bootstrapjsg.R")
 
 #bootstrapping athlete data
 bootstrapjsg(data1=sport[sport$Sex == "male", "Ht"], null=175.6)
--- lecture_files/6_2_sample_continuous tests.R
+++ lecture_files/6_2_sample_continuous tests.R
@@ -177,51 +177,10 @@ SIGN.test((insect_speciation$Polyandrous_species - insect_speciation$Monandrous_
 
 #bootstrap####
 
-#load bootstrapjsgfunction, which librarys simpleboot
-
+#load bootstrapjsgfunction, which librarys simpleboot and boot
 #bootstrapjsg function####
-bootstrapjsg <- function(data1, data2=NULL, conf=.95, fun=mean, r=10000, null=0)
-{
-  library(boot)
-  library(simpleboot)
-  one.boot <- function (data, FUN, R, student = FALSE, M, weights = NULL, ...) 
-  {
-    func.name <- ifelse(is.character(FUN), FUN, deparse(substitute(FUN)))
-    extra <- list(...)
-    if (func.name == "quantile") {
-      if (is.na(match("probs", names(extra)))) 
-        stop("'probs' argument must be specified")
-      if (length(extra$probs) > 1) 
-        stop("can only bootstrap a single quantile")
-    }
-    func <- match.fun(FUN)
-    boot.func <- function(x, idx) {
-      fval <- func(x[idx], ...)
-      if (student) {
-        rs.x <- x[idx]
-        b <- one.boot(rs.x, FUN, R = M, student = FALSE, 
-                      M = NULL, weights = NULL, ...)
-        fval <- c(fval, var(b$t))
-      }
-      fval
-    }
-    b <- boot(data, statistic = boot.func, R = R, weights = weights)
-    b$student <- student
-    structure(b, class = "simpleboot")
-  }
-  if (is.null(data2)){
-    a=one.boot(na.omit(data1), fun, r)
-    confint=boot.ci(a, conf)
-    p=length(subset(a$t, abs(a$t-mean(a$t))>=abs(null-mean(a$t))))/r
-    output=c(conf, "% Confidence Interval", confint$percent[1,4:5], "p-value", p)
-    return(output)}
-  if (is.null(data2)==F){
-    a=two.boot(na.omit(data1), na.omit(data2), fun, r)
-    confint=boot.ci(a, conf)
-    p=length(subset(a$t, abs(a$t-mean(a$t))>=abs(null-mean(a$t))))/r
-    output=c(conf, "% Percentile Confidence Interval", confint$percent[1,4:5], "p-value", p)
-    return(output)}
-}
+#source from github
+source("https://raw.githubusercontent.com/jsgosnell/CUNY-BioStats/master/code_examples/bootstrapjsg.R")
 
 
 #back to our aussie athlete data
