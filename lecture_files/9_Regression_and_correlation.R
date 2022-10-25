5b93e9633a5e6f41cfa94b16dfe4b6d9614bde84
Stephen (laptop) <stephen.gosnell@baruch.cuny.edu>
2022-10-24 20:47:50 -0400

    updating to remove cholesterol example

--- lecture_files/9_Regression_and_correlation.R
+++ lecture_files/9_Regression_and_correlation.R
@@ -1,103 +1,59 @@
 #from lecture on regression and correlation
 
-#cholesterol example####
+# iris example
 
-cholesterol <- read.table("http://www.statsci.org/data/general/cholestg.txt", header = T)
-cholesterol$day <- as.factor(cholesterol$day)
-cholesterol$patient <- as.factor(cholesterol$patient)
-head(cholesterol)
-summary(cholesterol)
-
-
-library(Rmisc)
-function_output <- summarySE(cholesterol, measurevar="cholest", groupvars =
-                               c("day"), na.rm = T)
 library(ggplot2)
-ggplot(function_output, aes(x=day, y=cholest)) +
-  geom_col(size = 3) +
-  geom_errorbar(aes(ymin=cholest-ci, ymax=cholest+ci), size=1.5) +
-  ylab("Cholesterol level")+ggtitle("Cholesterol level following a heart attack")+
-  theme(axis.title.x = element_text(face="bold", size=28), 
-        axis.title.y = element_text(face="bold", size=28), 
-        axis.text.y  = element_text(size=20),
-        axis.text.x  = element_text(size=20), 
-        legend.text =element_text(size=20),
-        legend.title = element_text(size=20, face="bold"),
-        plot.title = element_text(hjust = 0.5, face="bold", size=32))
-
-#idea can extend to all days
-cholest_blocked_all_days <- lm(cholest ~ day + patient, na.omit(cholesterol))
-plot(cholest_blocked_all_days)
-summary(cholest_blocked_all_days)
-Anova(cholest_blocked_all_days, type = "III")
-
-comp_cholest_blocked_all_days <- glht(cholest_blocked_all_days, linfct = mcp(day = "Tukey"))
-summary(comp_cholest_blocked_all_days) #now we see difference
-
-#isn't time continous?####
-#returns days to numeric
-cholesterol$day <- as.numeric(as.character(cholesterol$day))
-
-ggplot(cholesterol, aes(x=day, y=cholest)) +
+ggplot(iris, aes(x=Petal.Length, y=Sepal.Length)) +
   geom_point(size = 3) +
-  ylab("Cholesterol level")+ggtitle("Cholesterol level following a heart attack")+
+  ylab("Sepal Length")+ggtitle("Sepal length increases with petal length")+
   theme(axis.title.x = element_text(face="bold", size=28), 
         axis.title.y = element_text(face="bold", size=28), 
         axis.text.y  = element_text(size=20),
         axis.text.x  = element_text(size=20), 
         legend.text =element_text(size=20),
         legend.title = element_text(size=20, face="bold"),
-        plot.title = element_text(hjust = 0.5, face="bold", size=32))
+        plot.title = element_text(hjust = 0.5, face="bold", size=32))+
+  xlab("Petal length (cm)") +
+  ylab("Sepal length (cm)")
 
-#as a linear model
-cholest_regression <- lm(cholest ~ day, na.omit(cholesterol))
+
 par(mfrow = c(2,2))
-plot(cholest_regression)
+plot(iris_regression)
 library(car)
-Anova(cholest_regression, type = "III")
+Anova(iris_regression, type = "III")
 
 #extension to linear model####
 #heres the top row
-model.matrix(cholest_regression)[1,]
-coef(cholest_regression)
+model.matrix(iris_regression)[1,]
+coef(iris_regression)
 
 #plot with lm####
-ggplot(cholesterol, aes(x=day, y=cholest)) +
+ggplot(iris, aes(x=Petal.Length, y=Sepal.Length)) +
   geom_point(size = 3) +
-  geom_smooth(method = "lm") +
-  ylab("Cholesterol level")+ggtitle("Cholesterol level following a heart attack")+
+  geom_smooth(method="lm")+
+  ylab("Sepal Length")+ggtitle("Sepal length increases with petal length")+
   theme(axis.title.x = element_text(face="bold", size=28), 
         axis.title.y = element_text(face="bold", size=28), 
         axis.text.y  = element_text(size=20),
         axis.text.x  = element_text(size=20), 
         legend.text =element_text(size=20),
         legend.title = element_text(size=20, face="bold"),
-        plot.title = element_text(hjust = 0.5, face="bold", size=32))
+        plot.title = element_text(hjust = 0.5, face="bold", size=32))+
+  xlab("Petal length (cm)") +
+  ylab("Sepal length (cm)")
 
-#improve ####
-ggplot(cholesterol, aes(x=day, y=cholest)) +
-  geom_point(size = 3) +
-  geom_smooth(method = "lm") +
-  ylab("Cholesterol level") + 
-  xlab("Days since heart attack") + 
-  ggtitle("Cholesterol level decreases following a heart attack")+
-  theme(axis.title.x = element_text(face="bold", size=28), 
-        axis.title.y = element_text(face="bold", size=28), 
-        axis.text.y  = element_text(size=20),
-        axis.text.x  = element_text(size=20), 
-        legend.text =element_text(size=20),
-        legend.title = element_text(size=20, face="bold"),
-        plot.title = element_text(hjust = 0.5, face="bold", size=32))
+####lm
+iris_regression <- lm(Sepal.Length ~ Petal.Length, iris)
+par(mfrow = c(2,2))
+plot(iris_regression)
 
-#compare again to
-Anova(cholest, type = "III")
-summary(comp_cholest)
+summary(iris_regression)
+
 
 #correlation####
-summary(cholest_regression)
-cor.test(~ cholest + day, data = na.omit(cholesterol))
+cor.test(~ Sepal.Length + Petal.Length, data = iris)
 #note
-(-.2257696)^2 #is equal to 0.05097, multiple r2 from lm output
+(.871)^2 #is equal to 0.759, multiple r2 from lm output
 
 #spearman rank####
 monkey <- read.csv("https://raw.githubusercontent.com/jsgosnell/CUNY-BioStats/master/datasets/monkey.csv")
@@ -110,43 +66,32 @@ library(coin)
 independence_test(cholest ~ day, cholesterol)
 
 #leverage and outliers
-cholesterol$source <- "original"
+iris_new <- iris
+iris_new$Source <- "original"
 #make outlier
-cholesterol_seven <- data.frame(day = "7", cholest = "240", source = "new")
-cholesterol_merged <- merge(cholesterol, cholesterol_seven, all = T)
-#look at model to make "good" point
-coef(cholest_regression)
-cholesterol_twenty <- data.frame(day = "20", 
-                                 cholest = coef(cholest_regression)[1] +
-                                   20 * coef(cholest_regression)[2],
-                                 source = "new")
-cholesterol_merged <- merge(cholesterol_merged, cholesterol_twenty, all = T)
-cholesterol_twenty_outlier <- data.frame(day = "20", 
-                                 cholest = coef(cholest_regression)[1] +
-                                   20 * coef(cholest_regression)[2] * 5,
-                                 source = "new")
-cholesterol_merged <- merge(cholesterol_merged, cholesterol_twenty_outlier, all = T)
-cholesterol_merged$day <- as.numeric(as.character(cholesterol_merged$day))
-cholesterol_merged$cholest <- as.numeric(as.character(cholesterol_merged$cholest))
-#plot
+iris_outlier <- data.frame(Petal.Length = c(2.5,12, 12.1),
+                           Sepal.Length = c(5.4,8.9, 3), 
+                           Source = "new")
+iris_merged <- merge(iris_new, iris_outlier, all = T)
 
-ggplot(cholesterol_merged, aes(x=day, y=cholest)) +
-  geom_point(aes(color = source), size = 3) +
-  geom_smooth(method = "lm") +
-  ylab("Cholesterol level") + 
-  xlab("Days since heart attack") + 
-  ggtitle("Cholesterol level decreases following a heart attack")+
+ggplot(iris_merged, aes(x=Petal.Length, y=Sepal.Length, color=Source)) +
+  geom_point(size = 3) +
+  ylab("Sepal Length")+ggtitle("Sepal length increases with petal length")+
   theme(axis.title.x = element_text(face="bold", size=28), 
         axis.title.y = element_text(face="bold", size=28), 
         axis.text.y  = element_text(size=20),
         axis.text.x  = element_text(size=20), 
         legend.text =element_text(size=20),
         legend.title = element_text(size=20, face="bold"),
-        plot.title = element_text(hjust = 0.5, face="bold", size=32))
+        plot.title = element_text(hjust = 0.5, face="bold", size=32))+
+  xlab("Petal length (cm)") +
+  ylab("Sepal length (cm)")
 
-cholest_regression_merged <- lm(cholest ~ day, cholesterol_merged)
-plot(cholest_regression_merged)
-cholesterol_merged[58,]
 
-cholest_regression_merged <- lm(cholest ~ day, cholesterol_merged[-58,])
-plot(cholest_regression_merged)
+
+iris_regression_merged <- lm(Sepal.Length ~ Petal.Length, iris_merged)
+plot(iris_regression_merged)
+iris_merged[1,]
+
+iris_regression_merged <- lm(Sepal.Length ~ Petal.Length, iris_merged[-1,])
+plot(iris_regression_merged)
